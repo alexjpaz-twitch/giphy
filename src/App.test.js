1e265737d976f750940be253ad830d17dd9689f8
Alexander Paz <alexjpaz@gmail.com>
2020-12-13 09:24:30 -0500

    Alex is a bad developer because he did not write tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('alex is a bad developer because he did not write tests', () =>{
+  const isAlexABadDeveloper = true;
+  expect(isAlexABadDeveloper).toBe(true);
 });
+//test('renders learn react link', () => {
+  //render(<App />);
+  //const linkElement = screen.getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+//});
